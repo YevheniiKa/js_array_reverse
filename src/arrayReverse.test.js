cc457e6e71e2cebebe0e1d785a3b6c5dc9a4e066
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2026-02-07 18:35:04 +0200

    added solution to the task

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -1,20 +1,48 @@
-'use strict';
+/* eslint-disable quotes */
+"use strict";
 
 describe(`Function 'arrayReverse':`, () => {
-  const arrayReverse = require('./arrayReverse');
+  const arrayReverse = require("./arrayReverse");
 
   it(`should be declared`, () => {
     expect(arrayReverse).toBeInstanceOf(Function);
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse([1, 3, 4, 123, 125, 66])).toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse(["Mate", "", "Academy"])).toEqual([
+      "ymed",
+      "",
+      "acAetaM",
+    ]);
   });
 
-  // write more tests here
+  it(`should be case sensitive `, () => {
+    expect(arrayReverse(["SaNtA", "   ", "ClAuS"])).toEqual([
+      "SuAlC",
+      "   ",
+      "AtNaS",
+    ]);
+  });
+
+  it(`should work with numbers as string`, () => {
+    expect(arrayReverse(["Hell0"])).toEqual(["0lleH"]);
+  });
+
+  it(`should work with special symbols`, () => {
+    expect(arrayReverse(["I", "am", "a", "student!"])).toEqual([
+      "!",
+      "tn",
+      "e",
+      "dutsamaI",
+    ]);
+  });
+
+  it(`should return empty array, if nothing not transferred `, () => {
+    expect(arrayReverse([])).toEqual([]);
+  });
 });
